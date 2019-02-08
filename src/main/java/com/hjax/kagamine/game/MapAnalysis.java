201f3ec0131b1760d115b1b3ecd015c05677579e
Kevin Konrad <kevintkonrad@gmail.com>
2019-02-08 15:19:23 -0500

    improved overlord cliffs

--- src/main/java/com/hjax/kagamine/game/MapAnalysis.java
+++ src/main/java/com/hjax/kagamine/game/MapAnalysis.java
@@ -2,6 +2,7 @@ package com.hjax.kagamine.game;
 
 import java.util.ArrayList;
 
+import com.github.ocraft.s2client.protocol.debug.Color;
 import com.github.ocraft.s2client.protocol.spatial.Point2d;
 
 public class MapAnalysis {
@@ -12,21 +13,21 @@ public class MapAnalysis {
 	static {
 		Point2d min = Game.get_game_info().getStartRaw().get().getPlayableArea().getP0().toPoint2d();
 		Point2d max = Game.get_game_info().getStartRaw().get().getPlayableArea().getP1().toPoint2d();
-		for (int x = (int) min.getX(); x < max.getX(); x ++) {
-			for (int y = (int) min.getY(); y < max.getY(); y ++) {
-				pathable[x][y] = Game.pathable(Point2d.of(x, y));
-				height[x][y] = Game.height(Point2d.of(x, y));
+		for (int x = (int) min.getX(); x < max.getX() * 2; x ++) {
+			for (int y = (int) min.getY(); y < max.getY() * 2; y ++) {
+				pathable[x][y] = Game.pathable(Point2d.of((float) (x / 2.0), (float) (y / 2.0)));
+				height[x][y] = Game.height(Point2d.of((float) (x / 2.0), (float) (y / 2.0)));
 			}
 		}
-		for (int x = (int) min.getX(); x < max.getX(); x ++) {
-			for (int y = (int) min.getY(); y < max.getY(); y ++) {
+		for (int x = (int) min.getX(); x < max.getX() * 2; x ++) {
+			for (int y = (int) min.getY(); y < max.getY() * 2; y ++) {
 				boolean safe = true;
 				scan: for (int x_offset = -8; x_offset < 8; x_offset++) {
 					for (int y_offset = -8; y_offset < 8; y_offset++) {
 						if ((x_offset * x_offset + y_offset * y_offset) > 196) continue;
-						if (x + x_offset > max.getX() || x + x_offset < min.getX()) continue;
-						if (y + y_offset > max.getY() || y + y_offset < min.getY()) continue;
-						if (pathable[x + x_offset][y + y_offset] && height[x + x_offset][y + y_offset] > height[x][y] - 2) {
+						if (x + x_offset > max.getX() * 2 || x + x_offset < min.getX()) continue;
+						if (y + y_offset > max.getY() * 2 || y + y_offset < min.getY()) continue;
+						if (pathable[x + x_offset][y + y_offset] && height[x + x_offset][y + y_offset] > height[x][y] - 0.5) {
 							safe = false;
 							break scan;
 						}
@@ -36,25 +37,24 @@ public class MapAnalysis {
 			}
 		}
 		
-		for (int x = (int) min.getX(); x < max.getX(); x ++) {
-			inner: for (int y = (int) min.getY(); y < max.getY(); y ++) {
+		for (int x = (int) min.getX(); x < max.getX() * 2; x ++) {
+			inner: for (int y = (int) min.getY(); y < max.getY() * 2; y ++) {
 				if (air_safe[x][y]) {
-					for (int x_offset = -1; x_offset <= 1; x_offset++) {
-						for (int y_offset = -1; y_offset <= 1; y_offset++) {
-							if (x_offset != 0 && y_offset != 0) continue;
-							if (Math.abs(x_offset) + Math.abs(y_offset) == 2) continue;
+					for (int x_offset = -2; x_offset <= 2; x_offset++) {
+						for (int y_offset = -2; y_offset <= 2; y_offset++) {
+							if (y_offset * y_offset + x_offset * x_offset > 5) continue;
 							if (!air_safe[x + x_offset][y + y_offset]) continue inner;
 						}
 					}
 					for (Point2d p : overlord_spots) {
-						if (p.distance(Point2d.of(x, y)) < 5) {
+						if (p.distance(Point2d.of((float) (x / 2.0), (float) (y / 2.0))) < 5) {
 							continue inner;
 						}
 					}
 					for (int x_offset = -2; x_offset <= 2; x_offset += 4) {
 						for (int y_offset = -2; y_offset <= 2; y_offset += 4) {
 							if (pathable[x + x_offset][y + y_offset]) {
-								overlord_spots.add(Point2d.of(x, y));
+								overlord_spots.add(Point2d.of((float) (x / 2.0), (float) (y / 2.0)));
 								continue inner;
 							}
 						}
