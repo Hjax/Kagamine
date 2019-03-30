3aec77f42e3303c81ad9cee0fb8260b8a0654e42
Kevin Konrad <kevintkonrad@gmail.com>
2019-03-29 23:14:39 -0400

    fix ling bug

--- src/main/java/com/hjax/kagamine/unitcontrollers/zerg/Zergling.java
+++ src/main/java/com/hjax/kagamine/unitcontrollers/zerg/Zergling.java
@@ -31,24 +31,22 @@ public class Zergling {
 				return;
 			}
 		}
-		
-		
-		if (Game.get_frame() % 10 == 0) {
-			if (BaseDefense.assignments.containsKey(u.unit().getTag()) && BaseDefense.surroundCenter.containsKey(u.getTag())) {
-				if (BaseDefense.surroundCenter.get(u.getTag()).distance(BaseDefense.assignments.get(u.getTag())) < 1) {
-					Game.unit_command(u, Abilities.ATTACK, BaseDefense.assignments.get(u.getTag()));
-				} else {
-					Vector2d offset = Utilities.direction_to(Vector2d.of(BaseDefense.surroundCenter.get(u.getTag())), Vector2d.of(BaseDefense.assignments.get(u.getTag())));
-					Point2d result = Vector2d.of(BaseDefense.assignments.get(u.getTag())).add(offset.scale(6)).toPoint2d();
-					if (Game.pathable(result) && Math.abs(Game.height(result) - Game.height(u.unit().getPosition().toPoint2d())) < 0.5) {
-						Game.unit_command(u, Abilities.MOVE, result);
-					} else {
-						Game.unit_command(u, Abilities.ATTACK, BaseDefense.assignments.get(u.getTag()));
-					}
-				}
+
+
+		if (BaseDefense.assignments.containsKey(u.unit().getTag()) && BaseDefense.surroundCenter.containsKey(u.getTag())) {
+			if (BaseDefense.surroundCenter.get(u.getTag()).distance(BaseDefense.assignments.get(u.getTag())) < 1) {
+				Game.unit_command(u, Abilities.ATTACK, BaseDefense.assignments.get(u.getTag()));
 			} else {
-				GenericUnit.on_frame(u, true);
+				Vector2d offset = Utilities.direction_to(Vector2d.of(BaseDefense.surroundCenter.get(u.getTag())), Vector2d.of(BaseDefense.assignments.get(u.getTag())));
+				Point2d result = Vector2d.of(BaseDefense.assignments.get(u.getTag())).add(offset.scale(6)).toPoint2d();
+				if (Game.pathable(result) && Math.abs(Game.height(result) - Game.height(u.unit().getPosition().toPoint2d())) < 0.5) {
+					Game.unit_command(u, Abilities.MOVE, result);
+				} else {
+					Game.unit_command(u, Abilities.ATTACK, BaseDefense.assignments.get(u.getTag()));
+				}
 			}
+		} else {
+			GenericUnit.on_frame(u, true);
 		}
 	}
 }
