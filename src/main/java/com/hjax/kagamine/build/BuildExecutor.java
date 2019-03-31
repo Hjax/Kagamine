6351f83307eeabd39f67a9496350a5db13632552
Kevin Konrad <kevintkonrad@gmail.com>
2019-03-30 23:57:17 -0400

    no single safety spine

--- src/main/java/com/hjax/kagamine/build/BuildExecutor.java
+++ src/main/java/com/hjax/kagamine/build/BuildExecutor.java
@@ -78,7 +78,7 @@ public class BuildExecutor {
 			if (Game.army_supply() >= 2 && Game.army_supply() < 30 && BaseManager.base_count(Alliance.SELF) < 3) {
 				if (count(Units.ZERG_SPINE_CRAWLER) < 3 && !Wisdom.cannon_rush() && Build.scout) {
 					if (GameInfoCache.count_friendly(Units.ZERG_SPAWNING_POOL) > 0 && (BaseManager.base_count(Alliance.SELF) > 1 || Wisdom.proxy_detected())) {
-						if (Wisdom.all_in_detected() || Wisdom.proxy_detected() || (count(Units.ZERG_SPINE_CRAWLER) < 1 && Wisdom.aggression_detected() && EnemyModel.enemyBaseCount() < 2)) {
+						if (Wisdom.all_in_detected() || Wisdom.proxy_detected()) {
 							if (Game.can_afford(Units.ZERG_SPINE_CRAWLER)) {
 								BaseManager.build(Units.ZERG_SPINE_CRAWLER);
 							}
